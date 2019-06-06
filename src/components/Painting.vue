f104d60825cbb6580345f338c29feefc057e48ba
AlisonYoon <politescarlett1@gmail.com>
2019-06-06 17:03:14 +0100

    painting circle frame svg

--- src/components/Painting.vue
+++ src/components/Painting.vue
@@ -1,5 +1,31 @@
 <template>
-   <div>
-      <img src="https://dummyimage.com/600x400/000/fff" alt="">
-   </div>
+  <div id="js-paintingframe">
+    <svg
+      width="781px"
+      height="781px"
+      viewBox="0 0 781 781"
+      version="1.1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+    >
+      <g
+        id="Page-1"
+        stroke="none"
+        stroke-width="1"
+        fill="none"
+        fill-rule="evenodd"
+        stroke-opacity="0.499945367"
+      >
+        <g
+          id="Desktop-Copy"
+          transform="translate(-123.000000, -99.000000)"
+          stroke="#FFFFFF"
+          stroke-width="2"
+        >
+          <circle id="Oval" cx="513.5" cy="489.5" r="389.5"></circle>
+        </g>
+      </g>
+    </svg>
+    <img src="https://dummyimage.com/600x400/000/fff" alt>
+  </div>
 </template>
