a842712050f1862443bac59498a674a27df85ef7
AlisonYoon <politescarlett1@gmail.com>
2019-06-08 08:05:48 +0100

    svg deleted

--- src/components/Painting.vue
+++ src/components/Painting.vue
@@ -4,58 +4,6 @@
     <h1>Title</h1>
     <h3>Artist name</h3>
 
-<svg width="3px" height="1024px" viewBox="0 0 3 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd" stroke-opacity="0.6">
-        <g id="Desktop-Copy-2" transform="translate(-477.000000, 0.000000)" stroke="#FFFFFF" stroke-width="2">
-            <path d="M478.55142,0 L478.55142,1024" id="Path"></path>
-        </g>
-    </g>
-</svg>
-      <g
-        id="Page-1"
-        stroke="none"
-        stroke-width="1"
-        fill="none"
-        fill-rule="evenodd"
-        stroke-linecap="square"
-        stroke-opacity="0.596836757"
-      >
-        <g
-          id="Desktop-Copy-2"
-          transform="translate(-500.000000, 0.000000)"
-          stroke="#FFFFFF"
-          stroke-width="2"
-        >
-          <path d="M501,0 L501,1024" id="Line"></path>
-        </g>
-      </g>
-    </svg>
-    <svg
-      width="781px"
-      height="781px"
-      viewBox="0 0 781 781"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      xmlns:xlink="http://www.w3.org/1999/xlink"
-    >
-      <g
-        id="Page-1"
-        stroke="none"
-        stroke-width="1"
-        fill="none"
-        fill-rule="evenodd"
-        stroke-opacity="0.499945367"
-      >
-        <g
-          id="Desktop-Copy"
-          transform="translate(-123.000000, -99.000000)"
-          stroke="#FFFFFF"
-          stroke-width="2"
-        >
-          <circle id="Oval" cx="513.5" cy="489.5" r="389.5"></circle>
-        </g>
-      </g>
-    </svg>
     <img src="https://dummyimage.com/600x400/000/fff" alt>
   </div>
 </template>
